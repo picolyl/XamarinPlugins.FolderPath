9fc60154e0fc5d5ec495e2bd10deeb7ea049b8b7
picolyl <picolyl@gmail.com>
2016-04-17 19:00:13 +0900

    Add default interface

--- FolderPath/Plugin.FolderPath.Abstractions/IFolderPath.cs
+++ FolderPath/Plugin.FolderPath.Abstractions/IFolderPath.cs
@@ -7,5 +7,41 @@ namespace Plugin.FolderPath.Abstractions
     /// </summary>
     public interface IFolderPath
     {
+        /// <summary>
+        /// Application path
+        /// </summary>
+        string App { get; }
+        /// <summary>
+        /// Local data path
+        /// </summary>
+        string Local { get; }
+        /// <summary>
+        /// Roaming data path
+        /// </summary>
+        string Roaming { get; }
+        /// <summary>
+        /// Tamporary data path
+        /// </summary>
+        string Temporary { get; }
+        /// <summary>
+        /// Cache folder path
+        /// </summary>
+        string Cache { get; }
+        /// <summary>
+        /// Documents folder path
+        /// </summary>
+        string Documents { get; }
+        /// <summary>
+        /// Pictures folder path
+        /// </summary>
+        string Pictures { get; }
+        /// <summary>
+        /// Music folder path
+        /// </summary>
+        string Music { get; }
+        /// <summary>
+        /// Videos folder path
+        /// </summary>
+        string Videos { get; }
     }
 }
--- FolderPath/Plugin.FolderPath.UWP/FolderPathImplementation.cs
+++ FolderPath/Plugin.FolderPath.UWP/FolderPathImplementation.cs
@@ -8,5 +8,103 @@ namespace Plugin.FolderPath
     /// </summary>
     public class FolderPathImplementation : IFolderPath
     {
+        /// <summary>
+        /// Application path
+        /// </summary>
+        public string App
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Local data path
+        /// </summary>
+        public string Local
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Roaming data path
+        /// </summary>
+        public string Roaming
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Tamporary data path
+        /// </summary>
+        public string Temporary
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Cache folder path
+        /// </summary>
+        public string Cache
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Documents folder path
+        /// </summary>
+        public string Documents
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Pictures folder path
+        /// </summary>
+        public string Pictures
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Music folder path
+        /// </summary>
+        public string Music
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Videos folder path
+        /// </summary>
+        public string Videos
+        {
+            get
+            {
+                return null;
+            }
+        }
     }
 }
